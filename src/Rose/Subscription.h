3092e45843b385cd0be5285f1b4a6fa081aeaaee
Krzysztof Kucharski <k.l.kucharski@stud.elka.pw.edu.pl>
2019-05-05 23:22:29 +0200

    Add subscription container to accumulate subscriptions

--- src/Rose/Subscription.h
+++ src/Rose/Subscription.h
@@ -15,3 +15,48 @@ class SubscriptionInternal {
             removeAction();
         }
 };
+
+using Subscription = std::shared_ptr<SubscriptionInternal>;
+
+
+class SubscriptionContainer {
+    private:
+        std::vector<Subscription> subscriptions;
+
+    public:
+        SubscriptionContainer() = default;
+
+        void add(const Subscription & subscription) {
+            subscriptions.push_back(subscription);
+        }
+
+        void unsubscribe(const Subscription & subscription) {
+
+            int indexToRemove = -1;
+
+            for (int i = 0; i < subscriptions.size(); i++) {
+
+                auto ptr1 = subscriptions[i].get();
+                auto ptr2 = subscription.get();
+
+                if (!ptr1 || !ptr2) continue;
+
+                if (ptr1 == ptr2) {
+                    indexToRemove = i;
+                }
+            }
+
+            if (indexToRemove != -1) {
+                subscriptions[indexToRemove]->Unsubscribe();
+                subscriptions.erase(subscriptions.begin() + indexToRemove);
+            }
+        }
+
+        void unsubscribeAll() {
+            for (auto & s : subscriptions) {
+                s->Unsubscribe();
+            }
+
+            subscriptions.clear();
+        }
+};
