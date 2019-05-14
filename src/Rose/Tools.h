f53bf87d2e140854e31b5ad48a3719f71888b336
Krzysztof Kucharski <k.l.kucharski@stud.elka.pw.edu.pl>
2019-05-15 00:56:32 +0200

    Simplify removing shared ptr from vector of observers

--- src/Rose/Tools.h
+++ src/Rose/Tools.h
@@ -12,22 +12,7 @@ using Action = std::function<void()>;
 
 template<typename T>
 void Remove(std::vector<std::shared_ptr<T>> & vector, const std::shared_ptr<T> & t) {
-    /*
-    Remove shared_ptr from vector, where that shared_ptr points to same address as t because
-    shared pointers in vector are copies and we cannot remove them by referencing only t)
-    */
-    int index_to_remove = -1;
-
-    for (unsigned int i = 0; i < vector.size(); i++) {
-        if (vector[i].get() == t.get()) {
-            index_to_remove = i;
-        }
-    }
-
-    if (index_to_remove != -1) {
-        // After that only copy of shared pointer in vector will be removed
-        vector.erase(vector.begin() + index_to_remove);
-    }
+    vector.erase(std::find(vector.begin(), vector.end(), t));
 }
 
 template<typename T>
